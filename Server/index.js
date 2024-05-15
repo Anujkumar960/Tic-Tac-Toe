17412ea73d7325c669640cfa72adf5b3f4cc34d1
Anuj <anujkshatriya82@gmail.com>
2024-05-16 02:22:32 +0530

    crendial changed

--- Server/index.js
+++ Server/index.js
@@ -4,7 +4,7 @@ const { Server } = require("socket.io");
 const httpServer = createServer();
 const io = new Server(httpServer, {
   cors: {
-    origin: "https://nodejs-tic-tac-toe.netlify.app",
+    origin: "*",
     methods: ["GET", "POST"],
     credentials: true
   }
